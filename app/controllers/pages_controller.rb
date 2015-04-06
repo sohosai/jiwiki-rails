ff9e1da555ba9d79f6f6de584eac9d21b016fd66
polamjag <s@polamjag.info>
2015-04-06 23:46:39 +0900

    fix set_page bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -76,7 +76,7 @@ class PagesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_page
-    @page = Page.find_by(slug: params[:page_slug])
+    @page = Page.find(params[:slug])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
