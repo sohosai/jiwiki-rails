0d0b54510e6432cac63a35562379628c4afd8a5d
polamjag <s@polamjag.info>
2015-05-14 14:57:47 +0900

    refix find -> find_by

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -5,13 +5,13 @@ class VersionsController < ApplicationController
   end
 
   def new
-    @version = Version.new(page: Page.find(params[:page_slug]))
+    @version = Version.new(page: Page.find_by(slug: params[:page_slug]))
   end
 
   def create
     @version = Version.new
     @version.body = params[:version][:body]
-    @version.page = Page.find params[:page_slug]
+    @version.page = Page.find_by slug: params[:page_slug]
     @version.title = @version.page.title
     params[:slug] = params[:page_slug]
     if @version.save
